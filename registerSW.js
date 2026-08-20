15453910b6e90c5b9c0e927d956d32563b2826a1
王程民 <354026720@qq.com>
2026-08-20 20:54:06 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/self-trainer/sw.js', { scope: '/self-trainer/' })})}
